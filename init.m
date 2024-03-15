e5faa40617158a7b0ad788f96c993cefe64cf5fa
PrussianSpaceMarine <lobo.rosendahl@gmail.com>
2024-03-15 17:28:01 -0400

    Replaced .xlsx files with .csv
    .csv are smaller and more universal

--- init.m
+++ init.m
@@ -1,13 +1,13 @@
 %% Pull demographics and issues
 
 % Demographics use placeholders from Democracy 4
-dem = readtable("/MATLAB Drive/Polisim/settings/demographics.xlsx","ReadVariableNames",true);
+dem = readtable("/MATLAB Drive/Polisim/settings/demographics.csv","ReadVariableNames",true);
 
 % Issues are on a simple 2-axis political compass
-iss = readtable("/MATLAB Drive/Polisim/settings/issues.xlsx","ReadVariableNames",true);
+iss = readtable("/MATLAB Drive/Polisim/settings/issues.csv","ReadVariableNames",true);
 
 % Map demographics to their issues
-dem_iss = readtable("/MATLAB Drive/Polisim/settings/demographic_issues.xlsx","ReadVariableNames",true);
+dem_iss = readtable("/MATLAB Drive/Polisim/settings/demographic_issues.csv","ReadVariableNames",true);
 
 % Geographic/Polling Locations
-polls = readtable("/MATLAB Drive/Polisim/settings/polls.xlsx","ReadVariableNames",true);
+polls = readtable("/MATLAB Drive/Polisim/settings/polls.csv","ReadVariableNames",true);
