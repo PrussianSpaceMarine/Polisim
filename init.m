b594146ab41d0f49fb90cfb6e59cc3c9adc32b1d
PrussianSpaceMarine <lobo.rosendahl@gmail.com>
2024-03-15 02:30:24 +0000

    Add placeholder issues and mapped them to demographics

--- init.m
+++ init.m
@@ -2,3 +2,9 @@
 
 % Demographics use placeholders from Democracy 4
 dem = readtable("Polisim/settings/demographics.xlsx","ReadVariableNames",true);
+
+% Issues are on a simple 2-axis political compass
+iss = readtable("Polisim/settings/issues.xlsx","ReadVariableNames",true);
+
+% Map demographics to their issues
+dem_iss = readtable("Polisim/settings/demographic_issues.xlsx","ReadVariableNames",true);
