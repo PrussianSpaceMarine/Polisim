247f6c3a974142f18cd1c8e5de9786bf032cbecb
PrussianSpaceMarine <lobo.rosendahl@gmail.com>
2024-03-15 18:32:41 -0400

    Modified file structure to allow for multiple different data sets

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 Polisim is a simulator/game designed to realistically handle internal politics, with players representing parties.  It evolved out of a version based in Google Sheets (because PSM is a lunatic), and is now being moved to MatLab for more powerful, yet faster and simpler processing.
+
+Open and run "init.m" to get started.
--- init.m
+++ init.m
@@ -1,13 +1,16 @@
 %% Pull demographics and issues
 
+% Define which data set to use
+dataset = "default";
+
 % Demographics use placeholders from Democracy 4
-dem = readtable("/MATLAB Drive/Polisim/settings/demographics.csv","ReadVariableNames",true);
+dem = readtable("settings/datasets/" + dataset + "/demographics.csv","ReadVariableNames",true);
 
 % Issues are on a simple 2-axis political compass
-iss = readtable("/MATLAB Drive/Polisim/settings/issues.csv","ReadVariableNames",true);
+iss = readtable("settings/datasets/" + dataset + "/issues.csv","ReadVariableNames",true);
 
 % Map demographics to their issues
-dem_iss = readtable("/MATLAB Drive/Polisim/settings/demographic_issues.csv","ReadVariableNames",true);
+dem_iss = readtable("settings/datasets/" + dataset + "/demographic_issues.csv","ReadVariableNames",true);
 
 % Geographic/Polling Locations
-polls = readtable("/MATLAB Drive/Polisim/settings/polls.csv","ReadVariableNames",true);
+polls = readtable("settings/datasets/" + dataset + "/polls.csv","ReadVariableNames",true);
